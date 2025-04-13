57b3c9b3abc499419108bc8769906ce89d7f6788
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-04-13 05:26:48 -0600

    Adds solution to ch.3 titled 'The Makings Of A Programmer'

---
+++ part_1/ch_3/TheMakingsOfAProgrammer/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TheMakingsOfAProgrammer {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("The avarice never ends!");
+            Console.WriteLine("I want golf clubs.");
+            Console.WriteLine("I want diamonds.");
+            Console.WriteLine("I want a pony so I can ride it twice, get bored and sell it to make GLUE!");
+            Console.WriteLine("Blast this Christmas music! It's joyful and triumphant.");
+
+            /*
+             * Question: How many statements do you think a program can contain?
+             * 
+             * Answer: A program can contain an unlimited amount of statements!
+             */
+        }
+    }
+}
