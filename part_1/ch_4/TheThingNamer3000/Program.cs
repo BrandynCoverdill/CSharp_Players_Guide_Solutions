a1cfeb2710653017bed5709c67be02a2ee713db9
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2025-04-13 08:52:23 -0600

    Adds challenge 'TheThingNamer3000' to ch.4

---
+++ part_1/ch_4/TheThingNamer3000/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TheThingNamer3000 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("What kind of thing are we talking about?");
+
+            // 'a' stores the name of the thing.
+            string? a = Console.ReadLine();
+
+            Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+            /*
+             * 'b' stores the description of 'a'.
+             */
+            string? b = Console.ReadLine();
+
+            // Holds the string "of Doom".
+            string c = "of Doom";
+
+            /*
+             * Holds the string "3000".
+             */
+            string d = "3000";
+
+            Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
+        }
+    }
+}
+
+/*
+ * Question: Aside from the comments, what else could you do to make this code more understandable?
+ * 
+ * Answer: I would make the variable names more descriptive.
+ * 'a' could be 'nameOfThing' instead, 'b' could be 'description', and so on.
+ */
